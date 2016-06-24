b074c17bbb910bdfe4747c2bcf69f7a19468169c
Eduard <eduard.novembit@gmail.com>
2016-06-24 18:16:15 +0400

    Deleted environments

--- environments/index.php
+++ environments/index.php
@@ -29,56 +29,7 @@
  * ```
  */
 return [
-	'Local' => [
-        'path' => 'loc',
-        'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
-        ],
-        'setExecutable' => [
-            'yii',
-            //'tests/codeception/bin/yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
-        ],
-    ],
-    'Development' => [
-        'path' => 'dev',
-        'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
-        ],
-        'setExecutable' => [
-            'yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
-        ],
-    ],
-    'Production' => [
-        'path' => 'prod',
-        'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
-        ],
-        'setExecutable' => [
-            'yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
-        ],
-    ],
-    'Project' => [
+    'Local' => [
         'path' => 'proj',
         'setWritable' => [
             'backend/runtime',
