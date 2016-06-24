2f32b3c79afba58fe474f2d851a231376aafa126
Eduard <eduard.novembit@gmail.com>
2016-06-24 18:56:47 +0400

    Changes

--- common/components/widgets/Header.php
+++ common/components/widgets/Header.php
@@ -9,18 +9,6 @@ class Header extends \common\components\Widget{
     public function params(){
 
         $params = [
-            [
-                "slug" => "style",
-                "type" => "selectbox",
-                "label" => "Style",
-                "values" => [
-                    "1" => "Style 1",
-                    "2" => "Style 2",
-                    "3" => "Style 3",
-                    "4" => "Style 4",
-                ],
-                "default" => "1",
-            ]
         ];
 
         return $params;
